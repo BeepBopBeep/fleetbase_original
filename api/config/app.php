51e526a08f0af18bc370766300799a332213dee0
BeepBopBeep <66011171+BeepBopBeep@users.noreply.github.com>
2024-03-08 04:55:24 +0100

    Update app.php

--- api/config/app.php
+++ api/config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://138.197.184.125'),
 
     'asset_url' => env('ASSET_URL', null),
 
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'UTC+1',
 
     /*
     |--------------------------------------------------------------------------
